552594012772f4d1e3bbd1d832c94b0fe85dcf2f
fahad0100 <fahad.alazmi1994@gmail.com>
2025-12-01 18:57:58 +0300

    60

--- lib/get_content/content/features_files/presentation/screen_feature.dart
+++ lib/get_content/content/features_files/presentation/screen_feature.dart
@@ -5,11 +5,15 @@ String screenPageFeatureFile({required String featureName}) {
 
   return '''
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kids_app/features/$featureName/presentation/cubit/${featureName}_cubit.dart';
 
 class ${nameCab}FeatureScreen extends StatelessWidget {
   const ${nameCab}FeatureScreen({super.key});
   @override
   Widget build(BuildContext context) {
+      final _ = context.read<${nameCab}Cubit>();
+
     return Scaffold(
       appBar: AppBar(title: const Text('$nameCab Feature Screen')),
       body: Column(children: [
