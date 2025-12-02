1d1df1aa9410617c7bb8916cb183a847a171a888
fahad0100 <fahad.alazmi1994@gmail.com>
2025-12-02 14:01:10 +0300

    fox

--- lib/initialize/initialize_add_packages.dart
+++ lib/initialize/initialize_add_packages.dart
@@ -110,35 +110,35 @@ Future<void> _addPackagesBatch(
 //------------------------- packages dependencies ------------------------------
 
 const List<Map<String, dynamic>> corePackages = [
-  {"name": "flutter_dotenv", "version": '6.0.0'},
-  {"name": "multiple_result", "version": '5.2.0 '},
-  {"name": "flutter_bloc", "version": '9.1.1'},
-  {"name": "bloc", "version": '9.1.0'},
-  {"name": "dart_mappable", "version": '4.6.1'},
-  {"name": "dio", "version": '5.9.0'},
-  {"name": "retrofit", "version": '4.9.1'},
-  {"name": "easy_localization", "version": '3.0.8'},
-  {"name": "flutter_secure_storage", "version": '9.2.4'},
-  {"name": "sizer", "version": '3.1.3'},
-  {"name": "supabase_flutter", "version": '2.10.3'},
-  {"name": "get_storage", "version": '2.1.1'},
-  {"name": "get_it", "version": '9.1.1'},
-  {"name": "go_router", "version": '17.0.0'},
-  {"name": "injectable", "version": '2.7.0'},
-  {"name": "equatable", "version": '2.0.7'},
-  {"name": "package_info_plus", "version": '9.0.0'},
-  {"name": "device_info_plus", "version": '12.3.0'},
+  {"name": "flutter_dotenv", "version": '^6.0.0'},
+  {"name": "multiple_result", "version": '^5.2.0'},
+  {"name": "flutter_bloc", "version": '^9.1.1'},
+  {"name": "bloc", "version": '^9.1.0'},
+  {"name": "dart_mappable", "version": '^4.6.1'},
+  {"name": "dio", "version": '^5.9.0'},
+  {"name": "retrofit", "version": '^4.9.1'},
+  {"name": "easy_localization", "version": '^3.0.8'},
+  {"name": "flutter_secure_storage", "version": '^9.2.4'},
+  {"name": "sizer", "version": '^3.1.3'},
+  {"name": "supabase_flutter", "version": '^2.10.3'},
+  {"name": "get_storage", "version": '^2.1.1'},
+  {"name": "get_it", "version": '^9.1.1'},
+  {"name": "go_router", "version": '^17.0.0'},
+  {"name": "injectable", "version": '^2.7.0'},
+  {"name": "equatable", "version": '^2.0.7'},
+  {"name": "package_info_plus", "version": '^9.0.0'},
+  {"name": "device_info_plus", "version": '^12.3.0'},
 ];
 //------------------------- packages dev_dependencies --------------------------
 
 const List<Map<String, dynamic>> devPackages = [
-  {"name": "flutter_lints", "version": '6.0.0'},
+  {"name": "flutter_lints", "version": '^6.0.0'},
 
-  {"name": "build_runner", "version": '2.10.4'},
+  {"name": "build_runner", "version": '^2.10.4'},
 
-  {"name": "dart_mappable_builder", "version": '4.6.1'},
+  {"name": "dart_mappable_builder", "version": '^4.6.1'},
 
-  {"name": "retrofit_generator", "version": '10.2.0'},
+  {"name": "retrofit_generator", "version": '^10.2.0'},
 
-  {"name": "injectable_generator", "version": '2.11.0'},
+  {"name": "injectable_generator", "version": '^2.11.0'},
 ];
